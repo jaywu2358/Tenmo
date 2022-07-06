b81267095a34f91fb35e82f6eeb7164436c3fb86
Jonathan Ward <jward6t@gmail.com>
2022-07-06 15:44:47 -0500

    Adding AccountDao interface and placeholder JdbcAccountDao code

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,4 +1,20 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Account;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface AccountDao {
+
+    Account getAccountByAccountId(int accountId);
+
+    Account getAccountByUserId(int userId);
+
+    List<Account> listAllAccounts();
+
+    BigDecimal getBalanceByAccountId(int accountId);
+
+    BigDecimal getBalanceByUserId(int userId);
+
 }
--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,4 +1,30 @@
 package com.techelevator.tenmo.dao;
 
-public class JdbcAccountDao {
+import com.techelevator.tenmo.model.Account;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class JdbcAccountDao implements AccountDao {
+
+    public Account getAccountByAccountId(int accountId) {
+        return null;
+    }
+
+    public Account getAccountByUserId(int userId) {
+        return null;
+    }
+
+    public List<Account> listAllAccounts() {
+        return null;
+    }
+
+    public BigDecimal getBalanceByAccountId(int accountId) {
+        return null;
+    }
+
+    public BigDecimal getBalanceByUserId(int userId) {
+        return null;
+    }
+
 }
